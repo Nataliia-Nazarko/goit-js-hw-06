a6818e5c84dce2a626b7775b555f782a0d5db5f9
Nataliia Nazarko <nataliianazarko@gmail.com>
2022-01-15 00:22:00 +0200

    task 1+2

---
+++ src/js/task-01.js
@@ -0,0 +1,17 @@
+
+const numberOfCategoriesEl = document.querySelectorAll(".item");
+
+const titleCategoriesEl = document.querySelector("h2") 
+
+const categoryTitle = { Category: titleCategoriesEl.textContent}
+
+
+
+
+
+
+
+
+
+console.log(numberOfCategoriesEl);
+console.log(categoryTitle);
